d046b5d5d51150822b0f616494a95782a78e3170
timallenyi111 <Tim@arerectors.com>
2025-12-08 10:25:03 -0500

    rebasing local

--- obj/Debug/net8.0-windows/Inventor_Tools.AssemblyInfo.vb
+++ obj/Debug/net8.0-windows/Inventor_Tools.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("Inventor_Tools"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0d2ea69717b779927a7e30b974b157c890f4d2d"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+350a2918920ba03845a9c49f34c4e0d3a05c372b"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("Inventor_Tools"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("Inventor_Tools"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
